85b617790adead84557ae35a4188de6aa65231fb
2609-Prog <le.pucoc@uea.edu.ec>
2025-12-18 18:05:34 -0500

    Ordenar proyecto (gitignore)

--- obj/Debug/net10.0/CPE01.AssemblyInfo.cs
+++ obj/Debug/net10.0/CPE01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPE01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8999aca807317aebf34e0757c1dd749952ce7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPE01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPE01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
